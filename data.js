9837a93e557734bfb0360c2781da21dbce3af0f4
Rosa Vela <rosymer96@gmail.com>
2025-03-24 10:51:37 +0100

    Modificacion de imagenes y creacion de data.js con array de productos

---
+++ data.js
@@ -0,0 +1,19 @@
+const products = [
+    { id: 1, productName: "Aretes de Flor", src: "./Imagenes/imagen1.jpg", alt: "Aretes de Flor", description: "Aretes dorados con diseño floral", price: 12, stock: 14 },
+    { id: 2, productName: "Collar Carita Feliz", src: "./Imagenes/imagen2.jpg", alt: "Collar Carita Feliz", description: "Collar dorado con dije de carita sonriente", price: 18, stock: 13 },
+    { id: 3, productName: "Pulsera Morada Osito", src: "./Imagenes/imagen3.jpg", alt: "Pulsera Morada Osito", description: "Pulsera lila con dije de osito dorado", price: 15, stock: 12 },
+    { id: 4, productName: "Aretes Flor Lila", src: "./Imagenes/imagen4.jpg", alt: "Aretes Flor Lila", description: "Aretes dorados con flores lilas esmaltadas", price: 22, stock: 11 },
+    { id: 5, productName: "Collar Flor Lila", src: "./Imagenes/imagen5.jpg", alt: "Collar Flor Lila", description: "Collar con dije de flor lila esmaltada", price: 16, stock: 15 },
+    { id: 6, productName: "Aretes Arcoíris", src: "./Imagenes/imagen6.jpg", alt: "Aretes Arcoíris", description: "Aretes dorados con diseño de arcoíris colorido", price: 20, stock: 14 },
+    { id: 7, productName: "Collar Arcoíris", src: "./Imagenes/imagen7.jpg", alt: "Collar Arcoíris", description: "Collar con dije de arcoíris esmaltado", price: 13, stock: 12 },
+    { id: 8, productName: "Pulsera Arcoíris", src: "./Imagenes/imagen8.jpg", alt: "Pulsera Arcoíris", description: "Pulsera con cuentas coloridas y dije de arcoíris", price: 19, stock: 10 },
+    { id: 9, productName: "Aretes Osito", src: "./Imagenes/imagen9.jpg", alt: "Aretes Osito", description: "Aretes dorados en forma de osito", price: 14, stock: 13 },
+    { id: 10, productName: "Aretes Estrella", src: "./Imagenes/imagen10.jpg", alt: "Aretes Estrella", description: "Aretes con diseño de estrella dorada", price: 17, stock: 12 },
+    { id: 11, productName: "Collar Estrella", src: "./Imagenes/imagen11.jpg", alt: "Collar Estrella", description: "Collar con dije de estrella dorada", price: 21, stock: 11 },
+    { id: 12, productName: "Pulsera Rosa Osito", src: "./Imagenes/imagen12.jpg", alt: "Pulsera Rosa Osito", description: "Pulsera rosa con cuentas y dije de osito", price: 11, stock: 15 },
+    { id: 13, productName: "Aretes Unicornio", src: "./Imagenes/imagen13.jpg", alt: "Aretes Unicornio", description: "Aretes dorados con forma de unicornio", price: 23, stock: 14 },
+    { id: 14, productName: "Collar Corazón", src: "./Imagenes/imagen14.jpg", alt: "Collar Corazón", description: "Collar dorado con dije de corazón", price: 24, stock: 10 },
+    { id: 15, productName: "Aretes Mariposa", src: "./Imagenes/imagen15.jpg", alt: "Aretes Mariposa", description: "Aretes dorados con diseño de mariposa", price: 15, stock: 13 }
+  ];
+
+  
