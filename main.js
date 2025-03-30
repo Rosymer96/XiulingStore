034f26858e4ba26c387bcf5e0e9b746903c4069d
Rosa Vela <rosymer96@gmail.com>
2025-03-30 22:55:54 +0200

    Ajustes al vaciar el carrito y hacer la compra

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
 
 // Evento que despliega el menu oculto en responsive:
 
-document.addEventListener('DOMContentLoaded', showMenu);
+let $menuToggle = document.querySelector('.menuToggle');
+let $menu = document.querySelector('.menu');
+$menuToggle.addEventListener('click', showMenu);
+
 function showMenu(){
-  let $menuToggle = document.querySelector('.menuToggle');
-  let $menu = document.querySelector('.menu');
+  $menu.classList.toggle('active');
+  
+    //Ocultar el menu si se hace click en otro lugar (es decir No en el target)
 
-  $menuToggle.addEventListener('click', function(){
-    $menu.classList.toggle('active');
-  });
-
-    //Ocultar el menu si se hace click en otro lugar (el target)
   document.addEventListener('click', function(e){
     if(!$menu.contains(e.target) && !$menuToggle.contains(e.target)){
       $menu.classList.remove('active');
     }  
   });
-}
+};
+
 
 //Evento que despliega el carrito de compras al clickear el icono:
+let $shoppingCarIcon = document.querySelector('.shoppingCarIcon')
+let $shoppingCarContainer = document.querySelector('.shoppingCarContainer');
+$shoppingCarIcon.addEventListener('click', showShoppingCar);
 
-document.addEventListener('DOMContentLoaded', showShoppingCar);
 function showShoppingCar(){
-  let $shoppingCarIcon = document.querySelector('.shoppingCarIcon')
-  let $shoppingCar = document.querySelector('.shoppingCarContainer');
+  $shoppingCarContainer.classList.toggle('active');
+};
 
-  $shoppingCarIcon.addEventListener('click', function (){
-  $shoppingCar.classList.toggle('active');
-});
-}
 
 
 // Creacion de las pildoras con los productos:
@@ -39,102 +37,101 @@ $productContainer.classList.add('productsContainer');
 $main.appendChild($productContainer);
 
 for(let product of products){
-    let $product = document.createElement('div');
-    $productContainer.appendChild($product);
-    $product.classList.add('product-pill');
+  let $product = document.createElement('div');
+  $productContainer.appendChild($product);
+  $product.classList.add('product-pill');
 
-    $product.innerHTML= `
-    <div class= "product">
-      <div class= "containerImg">
-        <img class= "imgProduct" src="${product.src}" alt="${product.alt}"/>
-      </div>
-      <h3 class="nameProduct">${product.productName}</h3>
-      <p class="description">${product.description}</p>
-      <p class="price">€${product.price}</p>
-      <button id="${product.id}" class="btnProduct">Agregar al carrito</button>
+  $product.innerHTML= `
+  <div class= "product">
+    <div class= "containerImg">
+      <img class= "imgProduct" src="${product.src}" alt="${product.alt}"/>
     </div>
-    `;
+    <h3 class="nameProduct">${product.productName}</h3>
+    <p class="description">${product.description}</p>
+    <p class="price">€${product.price}</p>
+    <button data-id="${product.id}" class="btnProduct">Agregar al carrito</button>
+  </div>
+  `;
+}
 
-  }
+  //Añadir el evento a los btn creando un array de objetos seleccionados solo con el id, nombre y precio:
+let selectedProducts=[];
+let $shoppingCar = document.querySelector('.shoppingCar');
+let $totalPrice = document.querySelector('.totalPrice');
+let $btnProducts = document.querySelectorAll('.btnProduct');
 
-    //Añadir el evento a los btn creando un array de objetos seleccionados solo con el id, nombre y precio:
-  let selectedProducts=[];
-  let $shoppingCar = document.querySelector('.shoppingCar');
-  let $totalPrice = document.querySelector('.totalPrice');
-
-  let $btnProducts = document.querySelectorAll('.btnProduct');
-  for(let $btnProduct of $btnProducts){
-      $btnProduct.addEventListener('click', pushProduct);
-  }
-  function pushProduct(e){
-    // console.log(e);
-      let id = parseInt(e.target.id);
-      // Utilizando el e.target.id ya no es necesario esto(he borrado el id oculto en el productPill):
-      // let selectedProduct =e.target.closest('.product');
-      // if(selectedProduct){
-      //   let id = parseInt(selectedProduct.querySelector('.id').textContent); 
-      let existedProduct = selectedProducts.find(selected => selected.id === id);
-    // Verifica que me entregue el id correcto:
-    // console.log(id);
-    for(let product of products){
-      if(product.id === id){
-        if(!existedProduct){
-          selectedProducts.push({
-            quantity:1,
-            id: product.id,
-            name: product.productName,
-            price: product.price,
-            stock: product.stock,
-          });
-        } else if(existedProduct.stock <= existedProduct.quantity){
-            alert('Este producto no cuenta con stock suficiente.');
-        }
-        else{
-          existedProduct.quantity += 1;
-        }
-        break;
+for(let $btnProduct of $btnProducts){
+    $btnProduct.addEventListener('click', pushProduct);
+}
+function pushProduct(e){
+    console.log(e);
+    let id = parseInt(e.target.dataset.id);
+    let existedProduct = selectedProducts.find(selected => selected.id === id);
+  for(let product of products){
+    if(product.id === id){
+      if(!existedProduct){
+        selectedProducts.push({
+          quantity:1,
+          id: product.id,
+          name: product.productName,
+          price: product.price,
+          stock: product.stock,
+        });
+      } else if(existedProduct.stock <= existedProduct.quantity){
+          alert('Este producto no cuenta con stock suficiente.');
       }
+      else{
+        existedProduct.quantity += 1;
+      }
+      break;
     }
-  console.log(selectedProducts);
-
-  $shoppingCar.innerHTML = ' ';
-
-  for (let selectedProduct of selectedProducts ){
-      $selectedProduct = document.createElement('div');
-      $shoppingCar.appendChild($selectedProduct);
-      $selectedProduct.textContent = selectedProduct.name + ' x ' + selectedProduct.quantity;
-      $totalPrice.textContent = selectedProducts.reduce((acc, selectedProduct)=> acc + (selectedProduct.price * selectedProduct.quantity), 0);
   }
-  }; 
-    
-  
+console.log(selectedProducts);
 
-      // for(let product of products){
-      //   if (product.id === selectedId){
-      //     let quantity = 1;
-      //     selectedProducts.push(product.id, product.productName, product.price, product.stock, quantity);
-      //   }
-      // }
+$shoppingCar.innerHTML = ' ';
 
-      // let selectedProduct = e.target.closest('.product');
-      // if(selectedProduct){
-      //   let id = selectedProduct.querySelector('.id').textContent;
-      //   let name = selectedProduct.querySelector('.nameProduct').textContent;
-      //   let price = selectedProduct.querySelector('.price').textContent;
-      //   let counter = 1;
+for (let selectedProduct of selectedProducts ){
+    $selectedProduct = document.createElement('div');
+    $selectedProduct.classList.add('selectedProduct');
+    $shoppingCar.appendChild($selectedProduct);
+    $selectedProduct.innerHTML = `
+      <div>${selectedProduct.name}</div>
+      <div class="btns">
+      <button class="btnRemoveProduct">-</button><p class="quantity">${selectedProduct.quantity}</p><button class="btnAddProduct">+</button>
+      </div>
+    `;
+    // selectedProduct.name + ' x ' + selectedProduct.quantity;
+    $totalPrice.textContent = selectedProducts.reduce((acc, selectedProduct)=> acc + (selectedProduct.price * selectedProduct.quantity), 0);
+}
+}; 
 
-      //   let existedProduct = selectedProducts.find(product => product.id === id);
-      //   if(!existedProduct){
-      //     selectedProducts.push({counter,id,name,price});
-      //   } else{
-      //     existedProduct.counter += 1;
-      //   }
-      
-//Crear dinamicamente los div que se añaden al carrito de compras:
+//Añadir funcionalidad a los botones del carrito de compras:
 
+let $btnClearCar = document.querySelector('.btnClearCar');
+$btnClearCar.addEventListener('click', clearCar);
 
-    
+function clearCar(){
+  $shoppingCar.innerHTML = ' ';
+  selectedProducts = [];
+  $totalPrice.textContent = 0;
+}
 
+//Evento que permite generar la compra o envia mensaje en caso el carrito este vacio.
+let $btnStartOrder = document.querySelector('.btnStartOrder');
+let $messageOrder = document.querySelector('.messageOrder');
+$btnStartOrder.addEventListener('click', startOrder);
 
-    
+function startOrder(){
+  if(selectedProducts.length === 0){
+    alert('El carrito de compras esta vacío y no se puede proceder con la compra.');
+  } else{
+  $shoppingCarContainer.classList.remove('active');
+  $messageOrder.classList.toggle('active');
+  clearCar();
+  }
+}
+//Evento que cierra el mensaje de exito de compra.
+let $btnCloseMessageOrder = document.querySelector('.btnCloseMessageOrder');
+$btnCloseMessageOrder.addEventListener('click', () => $messageOrder.classList.remove('active'))
+
 
